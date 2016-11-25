a58cc98c4493018f820b777886a845363363682c
Santhii <santyjm_9616@hotmail.com>
2016-11-25 17:57:46 +0100

    Update ProyectoHospital.java

--- src/proyectohospital/ProyectoHospital.java
+++ src/proyectohospital/ProyectoHospital.java
@@ -16,6 +16,7 @@ public class ProyectoHospital {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("hshsgsus");
         // TODO code application logic here
     }
     
